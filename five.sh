75ca3d57cafc283e0bab641db5cc4fe2bab8cd33
sky <sky@qq.com>
2023-05-03 10:02:00 +0800

    case表达式

---
+++ five.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+case ${1} in
+    "one")
+    echo "what you input is one"
+    ;;
+    "two")
+    echo "what you input is two"
+    ;;
+    "three")
+    echo "what you input is three"
+    ;;
+esac
